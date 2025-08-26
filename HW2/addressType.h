d26e3f91ccb423b46b79df27c0a6fa02127ce35b
Aidsan58 <aidanr1@comcast.net>
2025-08-25 21:31:22 -0700

    Added library files for personType and addressType classes.

---
+++ HW2/addressType.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+class addressType {
+    public:
+        std::string streetAddress;
+        std::string city;
+        std::string state;
+        std::string zipCode;
+
+};
