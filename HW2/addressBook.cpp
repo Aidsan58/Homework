a5a5e54cd4137d0e55b3f21c142f63ab4b2b1005
Aidsan58 <aidanr1@comcast.net>
2025-08-30 11:27:07 -0700

    Implemented addressBookSort.

--- HW2/addressBook.cpp
+++ HW2/addressBook.cpp
@@ -1,5 +1,14 @@
 #include "addressBookType.h"
 
+void addressBookSort(addressBookType addressBook[]) { // Uses a lambda function with std::sort from the algorithm library to sort by last name.
+    std::sort(addressBook, addressBook + 500, [](const personType& a, const personType& b) {
+        return a.getLastName() < b.getLastName();
+    });
+}
+
 int main() {
-    
+    addressBookType addressBook[500];
+
+
+    return 0;
 }
