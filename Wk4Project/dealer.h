5d20ff0199ee6e32d8aa74d7702b51559e1f43e8
Aidsan58 <aidanr1@comcast.net>
2025-09-03 18:29:25 -0700

    Implemented shuffle function

--- Wk4Project/dealer.h
+++ Wk4Project/dealer.h
@@ -1,2 +1,23 @@
 // The dealer is a player
+#include <algorithm> // https://cplusplus.com/reference/algorithm/shuffle/
+#include <random> // default_random_engine generates pseudo random numbers.
+#include <chrono> // adds clock
 #include "player.h"
+#include "cardDeck.h"
+
+class dealer {
+    public:
+    bool canHit = true;
+    
+    private:
+    void shuffleDeck();
+    cardDeck deck;
+    
+};
+
+void dealer::shuffleDeck() {
+    
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count(); // I took this code from cplusplus.com
+    std::shuffle (deck.cards, deck.cards + cardDeck::CARDS_LENGTH, std::default_random_engine(seed));
+
+}
--- Wk4Project/player.h
+++ Wk4Project/player.h
@@ -4,5 +4,11 @@
 class player {
     public:
     std::string name;
+    static const int HAND_SIZE = 10;
+    int hand[HAND_SIZE];
+    private:
+    int handValue = 0;
+    bool canHit = true;
+
 
 };
