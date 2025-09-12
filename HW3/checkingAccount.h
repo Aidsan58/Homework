6c4574d2500025b21851490cc365a960e67ee9a0
Aidsan58 <aidanr1@comcast.net>
2025-09-12 15:29:26 -0700

    Implemented checkingAccount class

--- HW3/checkingAccount.h
+++ HW3/checkingAccount.h
@@ -1,3 +1,13 @@
 // serviceChargeChecking and noServiceChargeChecking are derived from here
 // Abstract class
 
+#ifndef CHECKING_ACCOUNT_H
+#define CHECKING_ACCOUNT_H
+
+#include "bankAccount.h"
+
+class checkingAccount : public bankAccount {
+    virtual void writeCheck() = 0;
+};
+
+#endif
