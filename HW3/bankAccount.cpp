6111bbe53dc678312acaca4bb49531bffeaeb6e8
Aidsan58 <aidanr1@comcast.net>
2025-09-12 15:25:35 -0700

    Implemented bankAccount class

---
+++ HW3/bankAccount.cpp
@@ -0,0 +1,13 @@
+#include "bankAccount.h"
+
+std::string bankAccount::getName() {
+    return name;
+}
+
+std::string bankAccount::getAccountNumber() {
+    return accountNumber;
+}
+
+double bankAccount::getBalance() {
+    return balance;
+}
--- HW3/bankAccount.h
+++ HW3/bankAccount.h
@@ -1,3 +1,26 @@
 // The one from which all others derive
 // Abstract class
 
+#ifndef BANK_ACCOUNT_H
+#define BANK_ACCOUNT_H
+
+#include <iostream>
+#include <string>
+
+class bankAccount {
+    public:
+    std::string name;
+    std::string accountNumber;
+    double balance;
+
+    std::string getName();
+    std::string getAccountNumber();
+    double getBalance();
+
+    virtual void makeDeposit() = 0;
+    virtual void withdrawMoney() = 0;
+    virtual void createMonthlyStatement() = 0;
+
+};
+
+#endif
