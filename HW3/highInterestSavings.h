a181811cd533088e08edad78c4f2b6341f1bfbe0
Aidsan58 <aidanr1@comcast.net>
2025-09-12 18:04:47 -0700

    Implemented savings classes

--- HW3/highInterestSavings.h
+++ HW3/highInterestSavings.h
@@ -1,1 +1,42 @@
 // Derived from savingsAccount
+
+#ifndef HIGH_INTEREST_SAVINGS_H
+#define HIGH_INTEREST_SAVINGS_H
+
+#include "savingsAccount.h"
+
+class highInterestSavings : public savingsAccount {
+    public:
+    double interest;
+
+
+    void withdrawMoney() override {
+        double withdrawalSize;
+        std::cout << "How much money do you wish to withdraw?" << std::endl;
+        std::cin >> withdrawalSize;
+        if ((balance - withdrawalSize) >= 0) { 
+            balance -= withdrawalSize;
+            std::cout << "Your account balance is now $" << balance << "." << std::endl;
+            withdrawalCount += 1;
+        }
+        else {  // If the withdrawal would bring the account balance to less than minimumBalance, the transaction is denied
+            std::cout << "You don't have enough funds to withdraw this cash." << std::endl;
+        }
+    }
+
+    void makeDeposit() override {
+        double depositSize;
+        std::cout << "How much money do you wish to deposit?" << std::endl;
+        std::cin >> depositSize;
+        balance += depositSize;
+        std::cout << "Your account balance is now $" << balance << "." << std::endl;
+        depositCount += 1;
+    }
+
+    void createMonthlyStatement() override {
+        std::cout << "You made " << withdrawalCount << " withdrawals and " << depositCount << " deposits." << std::endl;
+        std::cout << "Your account balance is now $" << (balance + interest) << "." << std::endl;
+    }
+};
+
+#endif
--- HW3/savingsAccount.h
+++ HW3/savingsAccount.h
@@ -1,1 +1,42 @@
 // Derives from bankAccount, highInterestSavings is derived from here
+
+#ifndef SAVINGS_ACCOUNT_H
+#define SAVINGS_ACCOUNT_H
+
+#include "bankAccount.h"
+
+class savingsAccount : public bankAccount {
+    public:
+    double interest;
+
+
+    void withdrawMoney() override {
+        double withdrawalSize;
+        std::cout << "How much money do you wish to withdraw?" << std::endl;
+        std::cin >> withdrawalSize;
+        if ((balance - withdrawalSize) >= 0) { 
+            balance -= withdrawalSize;
+            std::cout << "Your account balance is now $" << balance << "." << std::endl;
+            withdrawalCount += 1;
+        }
+        else {  // If the withdrawal would bring the account balance to less than minimumBalance, the transaction is denied
+            std::cout << "You don't have enough funds to withdraw this cash." << std::endl;
+        }
+    }
+
+    void makeDeposit() override {
+        double depositSize;
+        std::cout << "How much money do you wish to deposit?" << std::endl;
+        std::cin >> depositSize;
+        balance += depositSize;
+        std::cout << "Your account balance is now $" << balance << "." << std::endl;
+        depositCount += 1;
+    }
+
+    void createMonthlyStatement() override {
+        std::cout << "You made " << withdrawalCount << " withdrawals and " << depositCount << " deposits." << std::endl;
+        std::cout << "Your account balance is now $" << (balance + interest) << "." << std::endl;
+    }
+};
+
+#endif
