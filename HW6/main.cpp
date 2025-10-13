70c4e1a1cbe0945ac1ac7c9155ef81685e8793bc
Aidsan58 <aidanr1@comcast.net>
2025-10-13 10:38:16 -0700

    Added gitignore. Also changed main so that input has to be positive

--- HW6/main.cpp
+++ HW6/main.cpp
@@ -7,8 +7,15 @@ int main() {
     std::cout << "Enter the number of lines as an integer you want printed: ";
     std::cin >> myNum;
     std::cout << std::endl;
+    while (myNum < 1) { // If myNum isn't a positive integer, we prompt the user to try again
+        std::cout << "Not a valid input, the integer should be positive and nonzero. Try again: ";
+        std::cin >> myNum;
+        std::cout << std::endl;
+    }
 
     int goesDown = myNum;
     int goesUp = 0; // When goesDown reaches zero, then this number starts incrementing, that way we get the intended output with the stars
     starFunction(myNum, goesDown, goesUp);
+
+    return 0;
 }
