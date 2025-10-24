36bbe8c41a371f581fdb7f2ad79fe166be8deb37
Aidsan58 <aidanr1@comcast.net>
2025-10-24 13:36:20 -0700

    Fixed logic errors in main function

--- HW8/main.cpp
+++ HW8/main.cpp
@@ -14,9 +14,13 @@ int main() {
     stackType<int> myStack; // Instantiation of stackType with int type
     int myNum = decimalNum; // We don't want to change the value of decimalNum so we make myNum as a temp value
     while (myNum > 0) {
-        myNum = myNum / base; // First step in converting base 10 to base 2
-        myStack.push(myNum); // Add value to stack
-        std::cout << myStack.top() % base; // Return the modulus of the top of the stack divided by the base (which is 2, of course)
+        int remainder = myNum % base; // First step in converting base 10 to base 2. Return the modulus of the top of the stack divided by the base (which is 2, of course)
+        myStack.push(remainder); // Add value of remainder to stack
+        myNum = myNum / base; // Divide the value of myNum by 2
+    }
+    while (myStack.isEmptyStack() == false) { // We want to print in reverse of the order that we added
+        std::cout << myStack.top(); // Print the value at the top of the stack
+        myStack.pop(); // Pop the top from the stack and go back to the start of the loop until we have no more entries in the stack
     }
     std::cout << " binary" << std::endl;
     return 0;
