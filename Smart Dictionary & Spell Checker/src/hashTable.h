4d06e50481ecca89927f0f2a7298bcf7447566c1
Aidsan58 <aidanr1@comcast.net>
2025-12-07 15:41:38 -0800

    Hash set has public functions now

--- Smart Dictionary & Spell Checker/src/hashTable.h
+++ Smart Dictionary & Spell Checker/src/hashTable.h
@@ -5,8 +5,9 @@
 #include <string>
 
 struct HashSet {
+    private:
     std::unordered_set<std::string> table;   // store the words
-
+    public:
     void hashInsert(const std::string& entry);
     bool spellCheck(const std::string& word);
 };
