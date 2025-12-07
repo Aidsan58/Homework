8ab1958d9d5ff95eec8769dd2c6e43a213e81ddd
Aidsan58 <aidanr1@comcast.net>
2025-12-07 14:49:52 -0800

    Heap has public functions now

--- Smart Dictionary & Spell Checker/src/heap.h
+++ Smart Dictionary & Spell Checker/src/heap.h
@@ -5,6 +5,7 @@
 #include <iostream>
 
 class Heap {
+    public:
     void heapify(std::vector<std::string>& suggestion, int n, int i);
     void heapSort(std::vector<std::string>& suggestion);
     void printHeap(std::vector<std::string>& suggestion);
