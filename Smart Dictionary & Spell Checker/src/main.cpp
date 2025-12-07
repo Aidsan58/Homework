9a293927f462c8d2a0c2e0e24bc3b854a614dcd9
Aidsan58 <aidanr1@comcast.net>
2025-12-06 20:26:34 -0800

    Updated README

--- Smart Dictionary & Spell Checker/src/main.cpp
+++ Smart Dictionary & Spell Checker/src/main.cpp
@@ -2,14 +2,6 @@
 #include <vector>
 #include <unordered_set>
 
-struct CityRecord
-{
-    std:: string Name;
-    unsigned int Population;
-    double Latitude, Longitude;
-
-};
-
 int main() {
 
     std::unordered_set<std::string> hashSet;
