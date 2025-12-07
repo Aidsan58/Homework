045def81acd19bf1dfe07fe772c8887a83ff66fd
Aidsan58 <aidanr1@comcast.net>
2025-12-07 14:50:03 -0800

    Updated main

--- Smart Dictionary & Spell Checker/src/main.cpp
+++ Smart Dictionary & Spell Checker/src/main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+#include <fstream>
+#include "trie.h"
+#include "hashTable.h"
+#include "heap.h"
+
+
+void loadFile(const std::string& filename, HashSet& set, Trie& trie);
 
 int main() {
 
-    std::unordered_set<std::string> hashSet;
+    
+    HashSet set;
+    Trie trie;
+    Heap heap;
+
+    loadFile("dictionary.txt", set, trie);
+    std::vector<std::string> suggestion = trie.suggest("app");
+    heap.heapSort(suggestion);
 
     return 0;
+}
+
+void loadFile(const std::string& filename, HashSet& set, Trie& trie) {
+    std::ifstream inputFile(filename);
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Could not open the file: " << filename << std::endl;
+        return; // return early if we don't have a valid file or there is some other issue
+    }
+
+    std::unordered_set<std::string> hashSet; // hash set that we use to write to our class
+
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        set.hashInsert(hashSet, line);
+        trie.insert(line);
+    }
 }
