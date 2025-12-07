d38e8b59a2fbfcc00b7b6ec690a33ab093b4f8b1
Aidsan58 <aidanr1@comcast.net>
2025-12-07 15:36:21 -0800

    Added function for user input. Only LOAD is implemented for now

--- Smart Dictionary & Spell Checker/src/main.cpp
+++ Smart Dictionary & Spell Checker/src/main.cpp
@@ -8,6 +8,7 @@
 
 
 void loadFile(const std::string& filename, HashSet& set, Trie& trie);
+void processUserInput(HashSet& set, Trie& trie);
 
 int main() {
 
@@ -36,4 +37,30 @@ void loadFile(const std::string& filename, HashSet& set, Trie& trie) {
         set.hashInsert(line);
         trie.insert(line);
     }
+}
+
+void processUserInput(HashSet& set, Trie& trie) {
+    std::string input;
+
+    std::cout << ">";
+    std::getline(std::cin, input);
+
+    std::string command = input;
+
+    if (command.rfind("LOAD ", 0) == 0) {
+        std::string filename = input.substr(5); // the filename should be the characters after "LOAD "
+
+        // trim filename
+        filename.erase(0, filename.find_first_not_of("\t"));
+        filename.erase(filename.find_first_not_of("\t") + 1);
+
+        if (filename.empty()) {
+            std::cerr << "No filename found." << std::endl;
+            return;
+        }
+
+        loadFile(filename, set, trie);
+    }
+    else 
+        std::cout << "Unknown command." << std::endl;
 }
